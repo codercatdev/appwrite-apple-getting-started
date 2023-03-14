e3c19a64559a6d74affce33c6b016e97133ceaeb
Alex <alex@codingcat.dev>
2023-03-14 16:17:48 -0400

    Initial Commit

---
+++ Shared/Appwrite_Getting_StartedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Appwrite_Getting_StartedApp.swift
+//  Shared
+//
+//  Created by Alex Patterson on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Appwrite_Getting_StartedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
