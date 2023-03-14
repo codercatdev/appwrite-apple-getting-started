b42832de8b12c6ab909a4421682d9f801375a9c8
Alex <alex@codingcat.dev>
2023-03-14 17:23:22 -0400

    After Appwrite Countries Update

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,11 +6,39 @@
 //
 
 import SwiftUI
+import Appwrite
+
+let client = Client()
+    .setEndpoint("https://demo.appwrite.io/v1")
+    .setProject("6062f9c2c09ce")
+let locale = Locale(client);
 
 struct ContentView: View {
+    @State private var countries: [AppwriteModels.Country] = [];
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            List{
+                ForEach(self.countries, id: \.code){country in
+                    Text("\(country.name)")
+                }
+            }
+        }
+        VStack {
+            Button(action: {
+                Task {
+                    self.countries = try await locale.listCountries().countries
+                }
+                }) {
+                Text("Get Countries")
+            }
+            Button(action: {
+                Task {
+                    self.countries.removeAll()
+                }
+                }) {
+                Text("Remove")
+            }
+        }
     }
 }
 
